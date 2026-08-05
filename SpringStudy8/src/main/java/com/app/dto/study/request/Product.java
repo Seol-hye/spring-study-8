0e62c14f79d38e18f79d24132d0dc55b98bab493
Seol-hye <hs1507lll@gmail.com>
2026-08-05 16:45:20 +0900

    JSTL 주요기능 사용법

--- SpringStudy8/src/main/java/com/app/dto/study/request/Product.java
+++ SpringStudy8/src/main/java/com/app/dto/study/request/Product.java
@@ -9,7 +9,13 @@ public class Product {
 	public String name;
 	public int price;
 	
-	public String type;
+	public Product() {}
+	
+	public Product(String id, String name, int price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
 	
 //	public String getId() {
 //		return id;
