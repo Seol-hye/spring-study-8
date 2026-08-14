96ab662ff22e5ae480380198d7ce17df44235c5e
Seol-hye <hs1507lll@gmail.com>
2026-08-14 12:38:52 +0900

    관리자페이지 객실정보 수정처리

--- SpringStudy8/src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
+++ SpringStudy8/src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
@@ -58,4 +58,11 @@ public class RoomDAOImpl implements RoomDAO {
 		return result;
 	}
 
+	@Override
+	public int modifyRoom(Room room) {
+		
+		int result = sqlSessionTemplate.update("room_mapper.modifyRoom", room);
+		return result;
+	}
+
 }
