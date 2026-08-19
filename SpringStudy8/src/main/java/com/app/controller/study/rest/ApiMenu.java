e419454b8d0daca5ca078429b22b4a6e9bed17cd
Seol-hye <hs1507lll@gmail.com>
2026-08-19 14:30:28 +0900

    REST API 응답 response 객체 활용 (header, body 양식, 제네릭

---
+++ SpringStudy8/src/main/java/com/app/controller/study/rest/ApiMenu.java
@@ -0,0 +1,15 @@
+package com.app.controller.study.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiMenu {
+	
+	String name;
+	int price;
+
+}
