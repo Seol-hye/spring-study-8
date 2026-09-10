c09c1be40ea9c1aea2edebf5f523d189540f0d61
Seol-hye <hs1507lll@gmail.com>
2026-09-10 12:19:37 +0900

    [React Spring] React 로그인 API 처리, 세션 로그인 방식

--- SpringStudy8/src/main/java/com/app/common/ApiCommonCode.java
+++ SpringStudy8/src/main/java/com/app/common/ApiCommonCode.java
@@ -1,15 +1,18 @@
 package com.app.common;
 
-public class ApiCommonCode {
+public interface ApiCommonCode {
 	
-	public static String API_RESULT_SUCCESS = "10";
-	public static String API_RESULT_INNER_ERROR = "20";
-	public static String API_RESULT_FAIL_AUTH = "30";
-	public static String API_RESULT_EMPTY_DATA = "40";
+	public String API_RESULT_SUCCESS = "10";
+	public String API_RESULT_INNER_ERROR = "20";
+	public String API_RESULT_FAIL_AUTH = "30";
+	public String API_RESULT_EMPTY_DATA = "40";
 	
 
-	public static String API_RESULT_SUCCESS_MSG = "정상";
-	public static String API_RESULT_INNER_ERROR_MSG = "서버내부오류";
-	public static String API_RESULT_FAIL_AUTH_MSG = "인증실패";
-	public static String API_RESULT_EMPTY_DATA_MSG = "데이터없음";
+	public String API_RESULT_SUCCESS_MSG = "정상";
+	public String API_RESULT_INNER_ERROR_MSG = "서버내부오류";
+	public String API_RESULT_FAIL_AUTH_MSG = "인증실패";
+	public String API_RESULT_EMPTY_DATA_MSG = "데이터없음";
+	
+	public String API_LOGIN_SUCCESS = "100";
+	public String API_LOGIN_SUCCESS_MSG = "로그인 성공";
 }
---
+++ SpringStudy8/src/main/java/com/app/controller/study/rs/APILogin.java
@@ -0,0 +1,11 @@
+package com.app.controller.study.rs;
+
+import lombok.Data;
+
+@Data
+public class APILogin {
+	
+	String id;
+	String pw;
+
+}
